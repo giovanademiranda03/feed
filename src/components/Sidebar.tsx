ba7ee6c81b1d1a78c62fdfc98472ecb8c53b4cca
giovanademiranda03 <giovanademirandagithub@gmail.com>
2025-01-23 20:15:06 -0300

    feat: aadd component sidebar (profile)

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,25 @@
+import { PencilLine } from "phosphor-react";
+import { Avatar } from "./Avatar";
+import styles from "./Sidebar.module.css";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1517180102446-f3ece451e9d8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=40"
+      />
+      <div className={styles.profile}>
+        <Avatar src="https://github.com/giovanademiranda03.png" />
+        <strong>Giovana de Miranda</strong>
+        <span>Front-end Developer</span>
+      </div>
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
